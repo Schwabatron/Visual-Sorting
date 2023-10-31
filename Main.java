2fab9c8d24caaef4a2ca86bb8d1947c561383e40
Jack Schwabe <145683201+Schwabatron@users.noreply.github.com>
2023-10-31 09:19:17 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+
+
+		JFrame window = new JFrame(); //Creates a frame
+		
+		Frame_Info.make_frame(window);
+		
+	}
+	
+}
